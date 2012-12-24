49c8162fab68af55c17c87d0c9cd1fa63d23d4bc
Chris Wilson <chris@mfluent.com>
2012-12-24 07:54:35 -0600

    failed attempt at implementing binary log parsing. There appears to be a bug in the binary log data that adb emits. I see random bytes added to the stream.

--- LogCat/LogDatasource.m
+++ LogCat/LogDatasource.m
@@ -8,11 +8,16 @@
 #import "LogDatasource.h"
 #import "AdbTaskHelper.h"
 #import "NSString_Extension.h"
+#import "BinaryDataScanner.h"
 
 // 1 - adb logcat -v long
 // 2 - adb logcat -v threadtime
-// 3 - adb logcat -B  # Work in progress
+// 3 - adb logcat -B  # Work in progress: The binary streams seems to get corrupted and does not work reliably.
 #define LOG_FORMAT 2
+
+
+#define LOG_HEADER_LEN 20
+#define LOG_HEADER_LEN2 18
 
 @interface LogDatasource () {
     NSDate *startTime;
@@ -40,6 +45,8 @@
     NSString* type;
     NSString* name;
     NSMutableString* text;
+    
+    NSUInteger counter;
 }
 
 
@@ -77,6 +84,8 @@
         text = [NSMutableString stringWithCapacity:0];
         keysArray = [NSArray arrayWithObjects: KEY_TIME, KEY_APP, KEY_PID, KEY_TID, KEY_TYPE, KEY_NAME, KEY_TEXT, nil];
         isLogging = NO;
+        
+        counter = 0;
     }
     return self;
 }
@@ -341,13 +350,16 @@
             
             NSString *string;
             string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+//            NSLog(@"Data: %@", string);
             if (LOG_FORMAT == 1) {
                 [self performSelectorOnMainThread:@selector(appendLongLog:) withObject:string waitUntilDone:YES];
+                
             } else if (LOG_FORMAT == 2) {
-//                [self performSelectorOnMainThread:@selector(appendThreadtimeLog:) withObject:string waitUntilDone:YES];
                 [self appendThreadtimeLog:string];
+                
             } else if (LOG_FORMAT == 3) {
-                [self performSelectorOnMainThread:@selector(appendBinaryLog:) withObject:data waitUntilDone:YES];
+                [self appendBinaryLog:data];
+                
             }
         } else {
             NSLog(@"Data was nil...");
@@ -366,29 +378,179 @@
     NSLog(@"ADB Exited.");
 }
 
-- (void) appendBinaryLog: (NSData*) data {
-    NSAssert([NSThread isMainThread], @"Method can only be called on main thread!");
-    
-    NSLog(@"Read %ld bytes", [data length]);
-    /*
-     Header (20 bytes total):
-     [payloadlength]  2 bytes
-     [unused padding] 2 bytes
-     [PID]            4 bytes
-     [Thread ID]      4 bytes
-     [time seconds]   4 bytes
-     [time nanosecs]  4 bytes
-     [payload]        payloadlength bytes
-     
-     Payload section of the header is (payloadlength bytes total):
-     [log priority]            1 byte
-     [null terminated tag]     unknown length, < payloadlength
-     [null terminated log msg] unknown length, < payloadlength
-     */
-    
-     // TODO
-    
+- (void) logData:(NSData*) data {    
+    NSUInteger capacity = [data length] * 2;
+    NSMutableString *stringBuffer = [NSMutableString stringWithCapacity:capacity];
+    const unsigned char *dataBuffer = [data bytes];
+    NSInteger i;
+    for (i=0; i<[data length]; ++i) {
+        [stringBuffer appendFormat:@"%02lX", (NSUInteger)dataBuffer[i]];
+    }
+    NSLog(@"Data: %@", stringBuffer);
 }
+
+/**
+ The binary parsing code here is a mess. It seems adb gives a corrupted binary stream
+ */
+- (void) appendBinaryLog: (NSData*) data {
+//    NSAssert([NSThread isMainThread], @"Method can only be called on main thread!");
+//    
+//    if (counter > 40) {
+//        NSLog(@"End of test data..");
+//        return;
+//    }
+//    NSString* fileToLoad = [NSString stringWithFormat:@"/Users/chris/Desktop/binLogTest/binLog_%ld.bin", counter++];
+//    NSLog(@"Loading: %@", fileToLoad);
+//    data = [NSData dataWithContentsOfFile:fileToLoad];
+//    NSLog(@"Loaded: %ld bytes", [data length]);
+//    //    [data writeToFile:[NSString stringWithFormat:@"/Users/chris/Desktop/binLog_%ld.bin", counter++] atomically:NO];
+//
+//    
+////    NSLog(@"--------------------------------");
+////    NSLog(@"   Read %ld bytes", [data length]);
+////    NSLog(@"0-------------------------------");
+//
+//    /*
+//     Header (20 bytes total):
+//     [payloadlength]  2 bytes
+//     [unused padding] 2 bytes
+//     [PID]            4 bytes
+//     [Thread ID]      4 bytes
+//     [time seconds]   4 bytes
+//     [time nanosecs]  4 bytes
+//     [payload]        payloadlength bytes
+//     
+//     Payload section of the header is (payloadlength bytes total):
+//     [log priority]            1 byte
+//     [null terminated tag]     unknown length, < payloadlength
+//     [null terminated log msg] unknown length, < payloadlength
+//     */
+//    
+////    NSLog(@"PreData: (%ld)", [data length]
+////    [self logData:data];
+//    if (pendingData != nil && [pendingData length] > 0) {
+//        NSMutableData* newData = [NSMutableData dataWithData:pendingData];
+//        [newData appendData:data];
+//        data = newData;
+//        pendingData = nil;
+//    }
+//
+//
+////    data = [NSData dataWithContentsOfFile:@"/Users/chris/Desktop/testLog1msg.bin"];
+////    data = [NSData dataWithContentsOfFile:@"/Users/chris/Desktop/testBinLog2.bin"];
+//    
+//    NSLog(@"Post Data: (%ld)", [data length]);
+//    [self logData:data];
+//    BinaryDataScanner* binaryScanner = [BinaryDataScanner binaryDataScannerWithData:data littleEndian:YES defaultEncoding:NSUTF8StringEncoding];
+//
+//    NSUInteger bytesRead = 0;
+//    NSUInteger bufferOffset = 0;
+//    while ([binaryScanner remainingBytes] > LOG_HEADER_LEN) {
+//        NSUInteger size = [binaryScanner readWord];
+//        bytesRead = 2;
+//        NSLog(@"Size: %lu", size);
+//        
+//        NSLog(@"Remianing: %ld, Size: %ld", [binaryScanner remainingBytes], size+(LOG_HEADER_LEN));
+//        if (([binaryScanner remainingBytes]+bytesRead) < size+(LOG_HEADER_LEN)) {
+//            // We don't have a full log entry so save remaining buffer and get more
+//            
+//            NSUInteger offset = bufferOffset;
+//            NSUInteger len = [data length] - offset;
+//            NSRange range = NSMakeRange(offset, len);
+//            pendingData = [NSData dataWithData:[data subdataWithRange:range]];
+//            NSLog(@"1. Insufficient data. remainingBytes: %ld length: %ld (offset=%ld, len=%ld)", [binaryScanner remainingBytes], [data length], offset, len);
+//            return;
+//        }
+//        
+//        NSUInteger offset = bufferOffset;
+//        NSUInteger len = size + (LOG_HEADER_LEN);
+//        NSRange range = NSMakeRange(offset, len);
+//        NSData* dataToParse = [NSData dataWithData:[data subdataWithRange:range]];
+//        NSLog(@"Will Parse: (%ld)", size);
+//        [self logData:dataToParse];
+//
+//        [self parseBinaryLogEvent:dataToParse];
+//        
+//        bufferOffset += len;
+//        NSLog(@"Skipping: %ld", len);
+//        [binaryScanner skipBytes:len-bytesRead];
+//    
+//    }
+//
+//    if (pendingData == nil && [binaryScanner remainingBytes] > 0) {
+//        NSUInteger offset = bufferOffset;
+//        NSUInteger len = [data length] - offset;
+//        NSRange range = NSMakeRange(offset, len);
+//        pendingData = [NSData dataWithData:[data subdataWithRange:range]];
+//        NSLog(@"2. Insufficient data. remainingBytes: %ld length: %ld (offset=%ld, len=%ld)", [binaryScanner remainingBytes], [data length], offset, len);
+//        return;
+//    }
+}
+
+//- (void) parseBinaryLogEvent: (NSData*) data {
+//    /*
+//     The binary log cat seems to be putting random bytes into the stream so this does not work.
+//     */
+//    
+//    BinaryDataScanner* binaryScanner = [BinaryDataScanner binaryDataScannerWithData:data littleEndian:YES defaultEncoding:NSUTF8StringEncoding];
+//    NSUInteger size = [binaryScanner readWord];
+//    if (size == 0) {
+//        NSLog(@"Did not expect a size of zero");
+//    }
+//    
+//    NSUInteger padding = [binaryScanner readWord]; // unused padding
+//    if (padding != 0) {
+////        NSLog(@"Padding: %ld, %ld", padding, [binaryScanner remainingBytes]);
+//    }
+//    NSUInteger bPid = [binaryScanner readDoubleWord];
+////    NSLog(@"PID: %ld", bPid);
+//    pid = [NSString stringWithFormat:@"%ld", bPid];
+//    
+//    NSUInteger bTid = [binaryScanner readDoubleWord];
+////    NSLog(@"TID: %ld", bTid);
+//    tid = [NSString stringWithFormat:@"%ld", bTid];
+//    
+//    NSUInteger bSec = [binaryScanner readDoubleWord];
+////    NSLog(@"SEC: %ld", bSec);
+//    time = [NSString stringWithFormat:@"%ld", bSec];
+//    
+//    NSUInteger bnSec = [binaryScanner readDoubleWord];
+//    NSLog(@"NSEC: %ld", bnSec); // Log this so we don't get the compiler warning
+//    //        NSString* nanoSecs = [NSString stringWithFormat:@"%ld", bnSec];
+//    
+//    NSUInteger bPriority = [binaryScanner readByte];
+////    NSLog(@"Priority: %ld", bPriority);
+//    
+//    NSString* bTag = [binaryScanner readNullTerminatedString];
+////    NSLog(@"Tag: %@", bTag);
+//    
+//    NSString* bLogMsg = [binaryScanner readNullTerminatedString];
+////    NSLog(@"Msg: %@", bLogMsg);
+//    
+//    app = [self appNameForPid:pid];
+//    
+//    NSString* logLevel = [self logLevelForValue:bPriority];
+//    
+//    NSArray* lines = [bLogMsg componentsSeparatedByCharactersInSet: [NSCharacterSet newlineCharacterSet]];
+//    for (NSString* line in lines) {
+//        if ([line length] == 0) {
+//            continue;
+//        }
+//        
+//        if (bTag == nil) {
+//            bTag = @"";
+//        }
+//        NSLog(@"%@ %@ %@ %@ %@ %@ %@", time, app, pid, tid, logLevel, bTag, line != nil ? line : @"");
+//        NSArray* values = [NSArray arrayWithObjects: time, app, pid, tid, logLevel, bTag, line != nil ? line : @"", nil];
+//        if ([values count] < 7) {
+//            NSLog(@"Invalid Length. %ld", [values count]);
+//        }
+//        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:keysArray];
+//        [self appendRow:row];
+//    }
+//    
+//    [self performSelectorOnMainThread:@selector(onLogUpdated) withObject:nil waitUntilDone:YES];
+//}
 
 - (void) appendThreadtimeLog: (NSString*) paramString {
 //    NSAssert([NSThread isMainThread], @"Method can only be called on main thread!");
@@ -471,23 +633,7 @@
         return;
     }
     
-    NSString* appVal = [pidMap objectForKey:pidVal];
-    if (appVal == nil) {
-        NSLog(@"%@ not found in pid map.", pidVal);
-        [self loadPID];
-        appVal = [pidMap objectForKey:pidVal];
-        if (app == nil) {
-            // This is normal during startup because there can be log
-            // messages from apps that are not running anymore.
-            appVal = @"unknown";
-            NSTimeInterval elapsedTime = -[startTime timeIntervalSinceNow];
-            NSLog(@"Elapsed: %f", elapsedTime);
-            if (elapsedTime < 30) {
-                // There are potentially a lot of these when we first start reading the cached log
-                [pidMap setValue:appVal forKey:pidVal];
-            }
-        }
-    }
+    NSString* appVal = [self appNameForPid:pidVal];
     
     NSString* tidVal;
     result = [scanner scanUpToString:@" " intoString:&tidVal];
@@ -694,6 +840,29 @@
     }
 }
 
+- (NSString*) appNameForPid:(NSString*) pidVal {
+    NSString* appVal = [pidMap objectForKey:pidVal];
+    if (appVal == nil) {
+        NSLog(@"%@ not found in pid map.", pidVal);
+        [self loadPID];
+        appVal = [pidMap objectForKey:pidVal];
+        if (appVal == nil) {
+            // This is normal during startup because there can be log
+            // messages from apps that are not running anymore.
+            appVal = @"unknown";
+            NSTimeInterval elapsedTime = -[startTime timeIntervalSinceNow];
+            // NSLog(@"Elapsed: %f", elapsedTime);
+            if (elapsedTime < 30) {
+                // There are potentially a lot of these when we first start reading the cached log
+                [pidMap setValue:appVal forKey:pidVal];
+            }
+        }
+    }
+    
+    return appVal;
+    
+}
+
 - (NSInteger) getLogLevelForValue: (NSString*) logLevel {
     /*
      Java Log Levels:
@@ -728,6 +897,42 @@
         return 2;
     }
 }
+
+- (NSString*) logLevelForValue: (NSUInteger) level {
+    /*
+     Java Log Levels:
+     
+     public static final int VERBOSE = 2;
+     public static final int DEBUG = 3;
+     public static final int INFO = 4;
+     public static final int WARN = 5;
+     public static final int ERROR = 6;
+     public static final int ASSERT = 7;
+     */
+    switch (level) {
+        case 2:
+            return @"V";
+
+        case 3:
+            return @"D";
+
+        case 4:
+            return @"I";
+
+        case 5:
+            return @"W";
+
+        case 6:
+            return @"E";
+
+        case 7:
+            return @"A";
+
+        default:
+            return [NSString stringWithFormat:@"%ld", level];
+    }
+}
+
 
 - (BOOL)searchMatchesRow:(NSDictionary*)row
 {
